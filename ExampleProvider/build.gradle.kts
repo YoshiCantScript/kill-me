9ec6ee7e3ddb8fe71153f31f65bf1b4fc08a2495
YoshiCantScript <149406088+YoshiCantScript@users.noreply.github.com>
2024-02-03 20:29:16 +0100

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,38 +1,25 @@
-dependencies {
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.recyclerview:recyclerview:1.2.1")
-}
-// use an integer for version numbers
-version = -1
+version = 7
 
 
 cloudstream {
+    language = "it"
     // All of these properties are optional, you can safely remove them
 
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst")
+    // description = "Lorem Ipsum"
+    authors = listOf("Adippe","Forthe")
 
     /**
-    * Status int as the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta only
-    * */
-    status = 1
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+    )
 
-    tvTypes = listOf("NSFW")
-
-    requiresResources = true
-    language = "en"
-
-    // random cc logo i found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
-}
-
-android {
-    buildFeatures {
-        viewBinding = true
-    }
+    iconUrl = "https://www.google.com/s2/favicons?domain=streamingcommunity.online&sz=%size%"
 }
